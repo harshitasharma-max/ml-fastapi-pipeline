379eee7ff09491e60f0cf9a55717c4d21de106f8
harshitasharma-max <harshitarachnasharma@gmail.com>
2026-03-17 02:29:29 +0530

    pipeline.py

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,7 +1,49 @@
 import pandas as pd
-from app.model import LinearRegressionScratch
 
 model = None
+
+
+class LinearRegressionScratch:
+
+    def __init__(self):
+        self.weights = None
+        self.bias = 0
+
+    def fit(self, X, y, lr=0.00000001, epochs=1000):
+        n_samples = len(X)
+        n_features = len(X[0])
+
+        self.weights = [0.0] * n_features
+        self.bias = 0.0
+
+        for _ in range(epochs):
+
+            for i in range(n_samples):
+
+                prediction = sum(
+                    self.weights[j] * X[i][j] for j in range(n_features)
+                ) + self.bias
+
+                error = prediction - y[i]
+
+                for j in range(n_features):
+                    self.weights[j] -= lr * error * X[i][j]
+
+                self.bias -= lr * error
+
+    def predict(self, X):
+
+        predictions = []
+
+        for row in X:
+            pred = sum(
+                self.weights[i] * row[i] for i in range(len(row))
+            ) + self.bias
+
+            predictions.append(pred)
+
+        return predictions
+
 
 def train_model(file_path):
 
@@ -9,7 +51,10 @@ def train_model(file_path):
 
     data = pd.read_csv(file_path)
 
+    # Features = first columns
     X = data.iloc[:, :-1].values.tolist()
+
+    # Target = last column
     y = data.iloc[:, -1].values.tolist()
 
     model = LinearRegressionScratch()
@@ -20,6 +65,7 @@ def train_model(file_path):
 
 
 def predict(features):
+
     global model
 
     if model is None:
